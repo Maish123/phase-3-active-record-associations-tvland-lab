bba7b818fdb0b1295c1a61444a6affe7cb341dc6
Denis Maina <Denis.maina@student.moringaschool.com>
2022-09-01 18:19:13 +0300

    updated associations tvland lab

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,10 @@
 class Character < ActiveRecord::Base
+    belongs_to :actor 
+    belongs_to :show 
+
+    #SAY_THAT_THING_YOU_SAY
+    def say_that_thing_you_say
+        "#{self.name} always says: #{self.catchphrase}"
+    end
   
 end
